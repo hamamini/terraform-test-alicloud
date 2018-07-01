345ae8e82e996cd61477a23055cd876236710a15
Hamed <hamed@Hameds-MacBook-Air.local>
2018-07-01 11:50:21 +0430

    add variables

--- alicloud.tf
+++ alicloud.tf
@@ -7,39 +7,38 @@ provider "alicloud" {
 
 # Create a virtual private network
 resource "alicloud_vpc" "vpc-test" {
- name = "test"
- cidr_block = "10.1.0.0/24"
+ name = "${var.vpc_name}"
+ cidr_block = "${var.vpc_cidr_block}"
 }
 
 # Create a virtual subnet in the VPC
 resource "alicloud_vswitch" "vswitch-test" {
  vpc_id = "${alicloud_vpc.vpc-test.id}"
- cidr_block = "10.1.0.0/24"
- name = "test"
- availability_zone = "eu-central-1a"
+ cidr_block = "${var.vswitch_cidr_block}"
+ name = "${var.vswitch_name}"
+ availability_zone = "${var.availability_zone}"
  depends_on = ["alicloud_vpc.vpc-test"]
 }
 
 # Create a security group in the VPC
 resource "alicloud_security_group" "group-test" {
- name = "terraform-test-group"
- description = "New security group"
+ name = "${var.security_group_name}"
  vpc_id = "${alicloud_vpc.vpc-test.id}"
 }
 
 # Create an ecs instance in the virtual subnet
 resource "alicloud_instance" "instance-test" {
- image_id = "debian_9_02_64_20G_alibase_20171023.vhd"
- instance_type = "ecs.xn4.small"
+ image_id = "${var.image_id}"
+ instance_type = "${var.instance_type}"
  availability_zone = "${var.availability_zone}"
- count = "2"
+ count = "${var.number_of_instances}"
  security_groups = ["${alicloud_security_group.group-test.id}"]
  vswitch_id = "${alicloud_vswitch.vswitch-test.id}"
- system_disk_category = "cloud_efficiency"
- private_ip = "10.1.0.69"
- internet_max_bandwidth_out = "100"
- instance_name = "test"
- host_name = "testest"
- system_disk_size = "60"
- password= ""
+ system_disk_category = "${var.system_disk_category}"
+ private_ip = "${var.number_of_instances < 2 ? var.private_ip : ""}"
+ internet_max_bandwidth_out = "${var.internet_max_bandwidth_out}"
+ instance_name = "${var.number_of_instances < 2 ? var.instance_name : format("%s-%s", var.instance_name, format(var.number_format, count.index+1))}"
+ host_name = "${var.number_of_instances < 2 ? var.host_name : format("%s-%s", var.host_name, format(var.number_format, count.index+1))}"
+ system_disk_size = "${var.system_disk_size}"
+ password= "${var.instance_password}"
 }
--- variables.tf
+++ variables.tf
@@ -11,3 +11,51 @@ variable "region" {
 variable "availability_zone" {
   default = "eu-central-1a"
 }
+variable "vpc_name" {
+  default = "test"
+}
+variable "vpc_cidr_block" {
+	default = "10.1.0.0/16"
+}
+variable "vswitch_name" {
+  default = "test"
+}
+variable "vswitch_cidr_block" {
+	default = "10.1.0.0/16"
+}
+variable "security_group_name" {
+	default = "terraform-test-group"
+}
+variable "image_id" {
+	default = "debian_9_02_64_20G_alibase_20171023.vhd"
+}
+variable "instance_type" {
+	default = "ecs.xn4.small"
+}
+variable "number_of_instances" {
+	default     = "2"
+}
+variable "system_disk_category" {
+	default     = "cloud_efficiency"
+}
+variable "internet_max_bandwidth_out" {
+	default     = "100"
+}
+variable "instance_name" {
+	default     = "test"
+}
+variable "host_name" {
+	default     = "testest"
+}
+variable "system_disk_size" {
+	default     = "40"
+}
+variable "instance_password" {
+	default     = "123456"
+}
+variable "private_ip" {
+	default     = "10.1.0.69"
+}
+variable "number_format" {
+  default = "%02d"
+}
